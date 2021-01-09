5d8612772acb4a7c3564e25f377c9965c5de7837
Tanish Shah <stanish994@gmail.com>
2021-01-09 14:33:31 +0530

    Minutes Task

---
+++ MinutesToDaysAndYears.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class MinutesToDaysAndYears{
+
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+        int min;
+        System.out.print("Input the Number of Minutes: ");
+        min = sc.nextInt();
+        int years = min / 525600; // Number of Minuets in a year is 525600.
+        int MinRem = min % 525600;
+        int days = MinRem / 1440;
+        System.out.println(min + " minutes is approximately " + years + " years and " + days + " days.");
+        
+    
+    }
+}
